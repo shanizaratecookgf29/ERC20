a9250488bd389efe4153022a5d64662ec04a04cf
shanizaratecookgf29 <shanizaratecookgf29@hotmail.com>
2024-08-25 09:20:47 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("ttt","ttt"){} 
+    constructor()ERC20("ggg","ggg"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
